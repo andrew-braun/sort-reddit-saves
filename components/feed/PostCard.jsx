4386e2a16ee6f37bec7cf42309a06891c0701cfe
Andrew Braun <andrew@andrewbraun.dev>
2022-01-05 07:16:56 +0400

    feat: Set card image to choose large preview if available

--- components/feed/PostCard.jsx
+++ components/feed/PostCard.jsx
@@ -2,6 +2,22 @@ import Image from "next/image"
 import styles from "./post-card.module.css"
 
 export default function PostCard({ item }) {
+	const images = item.preview ? item.preview.images[0] : false
+
+	const imageProperties = {}
+
+	if (images) {
+		if (images.resolutions[3]) {
+			const res = images.resolutions[3]
+			imageProperties.source = res.url
+			imageProperties.width = res.width
+			imageProperties.height = res.height
+		} else {
+			imageProperties.source = images.source.url
+			imageProperties.width = images.source.width
+			imageProperties.height = images.source.height
+		}
+	}
 	return (
 		<article className={styles.postCard} key={item.name}>
 			{/* <p>{item.initialIndex}</p> */}
@@ -26,11 +42,11 @@ export default function PostCard({ item }) {
 				>
 					<Image
 						// src={item?.preview?.images[0]?.resolutions[1]?.url}
-						src={item.preview.images[0].source.url}
+						src={imageProperties.source}
 						alt={item.title}
 						className={styles.image}
-						width={item.preview.images[0].source.width}
-						height={item.preview.images[0].source.height}
+						width={imageProperties.width}
+						height={imageProperties.height}
 					/>
 				</a>
 			)}
